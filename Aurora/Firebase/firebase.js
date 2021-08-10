78a0a1e33ffdd055676625afa8b25d9195c1fec1
Joey <35291886+JoeyAlpha5@users.noreply.github.com>
2021-08-10 21:55:44 +0200

    Update firebase.js

--- Aurora/Firebase/firebase.js
+++ Aurora/Firebase/firebase.js
@@ -2,14 +2,7 @@ import * as firebase from 'firebase';
 import '@firebase/auth';
 var app;
 var firebaseConfig = {
-    apiKey: "AIzaSyCjDEHE_kuiaL3KQ0oWO9j6HwEaMb1j89g",
-    authDomain: "aurora-1a3a6.firebaseapp.com",
-    databaseURL: "https://aurora-1a3a6.firebaseio.com",
-    projectId: "aurora-1a3a6",
-    storageBucket: "aurora-1a3a6.appspot.com",
-    messagingSenderId: "429890650699",
-    appId: "1:429890650699:web:82b2695142b9d35d48c3ce",
-    measurementId: "G-86VT90PKY3"
+//     add your firebase settings here
 };
 if (!firebase.apps.length) {
     app = firebase.initializeApp(firebaseConfig);
